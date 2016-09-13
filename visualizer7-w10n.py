10d93b8543755a7e6d989b5b19e230d73b169b97
John Pham <johnphammail@gmail.com>
2016-09-12 18:15:28 -0700

    Added pom url

--- visualizer7-w10n.py
+++ visualizer7-w10n.py
@@ -7,6 +7,7 @@ layers_tfff = (
     )
 kmPerBlend = 10.
 renderLocation = None
+year, day = None, None  # day is in 365 format
 
 pomegrante =
-"http://airsl2.gesdisc.eosdis.nasa.gov/pomegranate/Aqua_AIRS_Level2/AIRX2RET.006/"
+"http://airsl2.gesdisc.eosdis.nasa.gov/pomegranate/Aqua_AIRS_Level2/AIRX2RET.006/{}/{}/AIRS.{}.{}.{}.L2.RetStd.v6.0.0.0.".format(year, day, year, month, granule)
