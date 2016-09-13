265f27f58969fa26c8a465c1398489590784943e
John Pham <johnphammail@gmail.com>
2016-09-13 10:26:49 -0700

    added download_data function

--- visualizer7-w10n.py
+++ visualizer7-w10n.py
@@ -1,13 +1,19 @@
-import bpy, random, argparse, sys, time, os, json
-from random import randint
-from enum import Enum
+try:
+    import bpy, requests, json
+except ImportError:
+    import requests, json
 
-layers_tfff = (
-    True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False
-    )
-kmPerBlend = 10.
-renderLocation = None
-year, day = None, None  # day is in 365 format
+longitude_url = (
+    'http://airsl2.gesdisc.eosdis.nasa.gov/pomegranate/Aqua_AIRS_Level2/AIRX2RET.006/2016/008/AIRS.2016.01.08.001.L2.RetStd.v6.0.31.0.G16008144115.hdf/Longitude[]?output=json')
 
-pomegrante =
-"http://airsl2.gesdisc.eosdis.nasa.gov/pomegranate/Aqua_AIRS_Level2/AIRX2RET.006/{}/{}/AIRS.{}.{}.{}.L2.RetStd.v6.0.0.0.".format(year, day, year, month, granule)
+def download_data(name, url):
+    # Downloads json from Pomegranate server then returns a list of values
+    print("Downloading {}...".format(name))
+    raw_data = requests.get(url)
+    data_json = json.loads(raw_data.text)
+    data_useful = data_json["data"]
+    data_flat = [item for sublist in data_useful for item in sublist]
+    return data_flat
+
+longitude = download_data("longitude", longitude_url)
+print(longitude[0])
