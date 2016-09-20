be1bfc1280ee1630d94f391d0ef3e853b5f8807f
John Pham <johnphammail@gmail.com>
2016-09-20 12:15:27 -0700

    need to authenticate Blender with desdisc

--- visualizer7-w10n.py
+++ visualizer7-w10n.py
@@ -1,9 +1,11 @@
 try:
     # Use when running within Blender
     import bpy, requests, json, datetime
+    blender = True
 except ImportError:
     # Use when outside of Blender
     import requests, json, datetime
+    blender = False
 
 debug = False    # Used to test functions without having to manually input
 
@@ -14,6 +16,7 @@ def convert_days(year, month, day):
     return day_of_year
 
 def search_path(url):
+    # Finds preliminary URL path
     print("Finding file...")
     raw_data = requests.get(url)
     data_json = json.loads(raw_data.text)
@@ -67,7 +70,73 @@ filename = search_path((url_prefix + search_pattern))
 # HTML view
 # print((url_prefix + filename + "/?output=HTML"))
 
-# Goes through dictionary and downloads corresponding data
-for key in granule_properties:
-    granule_properties[key] = download_data(key, (url_prefix + filename + "/" + key + "[]?output=json"))
-    print(len(granule_properties[key]))
+if blender == True:
+    # Goes through dictionary and downloads corresponding data
+    for key in granule_properties:
+        granule_properties[key] = download_data(key, (url_prefix + filename + "/" + key + "[]?output=json"))
+        print(len(granule_properties[key]))
+
+    # Parameters
+    outTag = ""
+    horizDecim = 1
+    vertMag = 1
+    animLen = 10.0
+    globe = False
+    stillCamera = False
+    lowRenderSettings = False
+    circularAnimation = False
+    spiralAnimation = False
+    topDownAnimation = False
+    thickbytype = False
+    cloudColor = 'type'
+    planeColor = 'blue'
+    sizeByFrac = None
+    reduceOverlap = True
+    cloudSolid = False
+    blendOut = False
+
+    kmPerBlend = 10.0
+    satHeight_km = 715.0
+    nadirRadKm = 15.0
+    kmPerBlendZ = kmPerBlend /  vertMag
+    fracMult = 0.9
+    thinBlend = 0.05 / kmPerBlend
+
+    # Creates first instance of cylinder to be copied later
+    bpy.ops.mesh.primitive_cylinder_add(radius=(0.5), depth=(1),
+                                    view_align=False, enter_editmode=False, location=(0, 0, 0))
+    # isc = latitude
+    for granule_properties["Latitude"][i] in range(45):
+        ysc_km = (granule_properties["Latitude"][i] - 45.0 / 2.0) * 45.0
+        ysc = ysc_km / kmPerBlend
+        # ifp = longitude
+        for granule_properties["Longitude"][i] in range(30):
+            scanang_deg = (granule_properties["Longitude"][i] - 14.5) * 3.3
+            scanang_rad = np.radians(scanang_deg)
+            xfp_km = satHeight_km * np.tan(scanang_rad)
+            xfp = xfp_km / kmPerBlend
+
+            # Copies mesh data of cylinder
+            if bpy.context.scene.objects.active.name != "Cylinder":
+                obj = bpy.context.scene.objects.active.copy()
+                obj.data = bpy.context.scene.objects.active.data.copy()
+                bpy.context.scene.objects.link(obj)
+            hmag = 1.0 / np.cos(scanang_rad)
+            xelong = 1.0 / np.cos(scanang_rad)
+
+            # Removes any materials from the cylinder
+            for material in range(len(bpy.context.active_object.data.materials)):
+                try:
+                    bpy.context.active_object.data.materials.pop()
+                except RuntimeError:
+                    pass
+            bpy.context.object.scale[0] = (3.0 * horizDecim * fracMult * hmag * xelong * nadirRadKm / kmPerBlend)
+            bpy.context.object.scale[1] = 3.0 * horizDecim * fracMult * nadirRadKm / kmPerBlend
+            bpy.context.object.scale[2] = thinBlend
+
+            bpy.context.scene.objects.active.location = (granule_properties["Longitude"][i], granule_properties["Latitude"][i], 0.0)
+
+            if bpy.context.scene.objects.active.name == "Cylinder":
+                obj = bpy.context.scene.objects.active.copy()
+                obj.data = bpy.context.scene.objects.active.data.copy()
+                bpy.context.scene.objects.link(obj)
