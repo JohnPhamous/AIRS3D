4f8be5f541f66908a1ed154f1ae8068f0b000466
John Pham <johnphammail@gmail.com>
2016-09-20 11:33:02 -0700

    downloads all needed fields

--- visualizer7-w10n.py
+++ visualizer7-w10n.py
@@ -5,6 +5,8 @@ except ImportError:
     # Use when outside of Blender
     import requests, json, datetime
 
+debug = False    # Used to test functions without having to manually input
+
 def convert_days(year, month, day):
     # Converts the user's date into the day of the year (1-365/366)
     user_date = datetime.datetime(year, month, day)
@@ -28,56 +30,44 @@ def download_data(name, url):
     data_flat = [item for sublist in data_useful for item in sublist]
     return data_flat
 
-
-
 # Dictionary with AIRS data fields
-# Not on Pomegranate: cloud_phase_3x3, ice_cld_opt_dpth ice_cld_eff_diam,
-# ice_cld_temp_eff, H2OMMRLevSup
 granule_properties = {"Longitude", "Latitude","TSurfStd", "TSurfAir",
-                      "CldFrcStd", "nCld", "PCldTop", "TCldTop"}
+                      "CldFrcStd", "nCld", "PCldTop", "TCldTop",
+                      "cloud_phase_3x3", "ice_cld_opt_dpth", "ice_cld_eff_diam",
+                      "ice_cld_temp_eff", "H2OMMRLevSup"}
 
 # Populates keys with None value
 granule_properties = {key: None for key in granule_properties}
 
-debug = True    # Used to test functions without having to manually input
-
 if debug == True:
     month = 10
     day = 25
-    year = 2007
+    year = 2010
     granule = 42
 else:
     print("Enter granule info below")
     month = input("Month(1-12): ")
+    month = "{0}".format(str(month).zfill(2))
     day = input("Day(1-31): ")
+    day = "{0}".format(str(day).zfill(2))
     year = str(input("Year(XXXX): "))
     granule = input("Granule: ")
     granule = "{0}".format(str(granule).zfill(3))
 
-# TODO fills are not working
-day = int("{0}".format(str(day).zfill(2)))   # Makes sure the day is in XX
-month = int("{0}".format(str(month).zfill(2)))   # Makes sure the month is in XX
 print("Searching for granule {} on {}-{}-{}".format(granule, month, day, year))
-day_of_year = convert_days(year, month, day)
+day_of_year = convert_days(int(year), int(month), int(day))
 
-# Finds the correct url for user's granule
-url_prefix = ("http://airsl2.gesdisc.eosdis.nasa.gov/pomegranate/Aqua_AIRS_Level2/AIRX2RET.006/{}/{}/".format(year,
+# Finds URL for supporting data of granule
+url_prefix = ("http://airsl2.gesdisc.eosdis.nasa.gov/pomegranate/Aqua_AIRS_Level2/AIRX2SUP.006/{}/{}/".format(year,
                                                                                                  day_of_year))
-search_pattern = "AIRS.{}.{}.{}.001.L2.RetStd.v6.0.7.0.**.hdf/?output=json".format(year,
+search_pattern = "AIRS.{}.{}.{}.001.L2.RetSup.v6.0.7.0.**.hdf/?output=json".format(year,
                                                                      month,
                                                                      day)
-search_url = url_prefix + search_pattern
-filename = search_path(search_url)
+filename = search_path((url_prefix + search_pattern))
+# HTML view
+# print((url_prefix + filename + "/?output=HTML"))
 
-
-def main():
-    for key in granule_properties:
-        # Downloads data for all keys in dictionary
-        granule_properties[key] = download_data(key, longitude_url)
-
-    # granule_properties["longitude"] = download_data("longitude", longitude_url)
-    print(granule_properties["longitude"][0])
-
-    for i in range(len(granule_properties["longitude"])):
-        # TODO: Make stuff here
-        pass
+# Goes through dictionary and downloads corresponding data
+for key in granule_properties:
+    granule_properties[key] = download_data(key, (url_prefix + filename + "/" + key + "[]?output=json"))
+    print(len(granule_properties[key]))
